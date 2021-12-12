7da7012161644b72b125258379ccc78312f86c1a
ltlx <ltlx4@icloud.com>
2021-12-12 09:35:25 +0100

    Added delete and create views in urls.py

--- films/urls.py
+++ films/urls.py
@@ -6,11 +6,14 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path("register/", views.RegisterView.as_view(), name="register")
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("movies/", views.MovieList.as_view(), name="movie-list"),
 ]
 
 htmx_urlpatterns = [
-    path('check_username/', views.check_username, name='check-username')
+    path('check_username/', views.check_username, name='check-username'),
+    path('add_movie/', views.add_movie, name='add-movie'),
+    path('delete_movie/<int:pk>/', views.delete_movie, name='delete-movie'),
 ]
 
 urlpatterns += htmx_urlpatterns
