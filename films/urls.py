a42d76c264da40cbb44ca4fcd454667d5034d1d5
ltlx <ltlx4@icloud.com>
2021-12-12 07:29:44 +0100

    Change root index to blank

--- films/urls.py
+++ films/urls.py
@@ -3,7 +3,7 @@ from films import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('index/', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path("register/", views.RegisterView.as_view(), name="register")
