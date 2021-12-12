be38f5b3c5bf756b160b585fffe30bde4dc8108d
ltlx <ltlx4@icloud.com>
2021-12-12 09:34:36 +0100

    Add a Movie model

--- films/models.py
+++ films/models.py
@@ -3,3 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Movie(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    users = models.ManyToManyField(User, related_name='movies')
+
+
+    def __str__(self):
+        return self.name or str(self.name)
